870b40372ce8ae3b9c8646ae7c6c3df6a19bb54a
Kevin Le Perf <codlabtech@gmail.com>
2016-04-13 14:32:31 +0200

    update nullable

--- app/src/main/java/eu/codlab/amiiwrite/events/PostRefreshAmiibos.java
+++ app/src/main/java/eu/codlab/amiiwrite/events/PostRefreshAmiibos.java
@@ -1,5 +1,8 @@
 package eu.codlab.amiiwrite.events;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 import java.util.List;
 
 import eu.codlab.amiiwrite.webservice.models.AmiiboDescriptorInformation;
@@ -8,9 +11,10 @@ import eu.codlab.amiiwrite.webservice.models.AmiiboDescriptorInformation;
  * Created by kevinleperf on 03/11/2015.
  */
 public class PostRefreshAmiibos {
+    @Nullable
     public List<AmiiboDescriptorInformation> amiibos;
 
-    public PostRefreshAmiibos(List<AmiiboDescriptorInformation> amiibos) {
+    public PostRefreshAmiibos(@Nullable List<AmiiboDescriptorInformation> amiibos) {
         this.amiibos = amiibos;
     }
 }
